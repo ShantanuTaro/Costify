12d0a679c4531ed380a65643c01b4e0d3be49b0c
ShantanuTaro <shantanutaro@gmail.com>
2025-08-06 22:18:03 +0530

    Initial Commit

---
+++ Costify/CostifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostifyApp.swift
+//  Costify
+//
+//  Created by Shantanu Taro on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CostifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CostifyTests/CostifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CostifyTests.swift
+//  CostifyTests
+//
+//  Created by Shantanu Taro on 06/08/25.
+//
+
+import Testing
+@testable import Costify
+
+struct CostifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
